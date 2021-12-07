82c4b07cbf5fce7aa52ec96199b0ef915e593b45
Ben Hardy <hardybe92@gmail.com>
2021-12-07 15:41:05 -0600

    finished part 7. it took like 10 minutes instead of the couple hours part 6 took.

---
+++ day7/day7.py
@@ -0,0 +1,10 @@
+with open('input.txt', 'r') as f:
+	nums = list(map(int, f.read().split(',')))
+	print(nums)
+
+	furthest = max(nums)
+	fuel_loads = [0] * furthest
+
+	for i in range(furthest):
+		fuel_loads[i] = sum([abs(x - i) for x in nums])
+	print(min(fuel_loads))
---
+++ day7/day7p2.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+with open('input.txt', 'r') as f:
+
+	nums = list(map(int, f.read().split(',')))
+
+	furthest = max(nums)
+	fuel_loads = [0] * furthest
+
+	# to handle the part 2 fuel loads, we're going to pre-compute them to save time
+	# rather than computing each one individually.
+
+	# it's just a cumulative sum so I'm going to use numpy to make it easier
+	fuel_per_distance = np.cumsum(np.array(list(range(furthest + 1)), int))
+
+	# now we can just use fuel_per_distance as a lookup table for each crab's distance
+	for i in range(furthest):
+		fuel_loads[i] = sum([fuel_per_distance[abs(x - i)] for x in nums])
+	print(min(fuel_loads))
