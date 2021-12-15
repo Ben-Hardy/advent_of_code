67cc3ae9b0d7664ec7ae649a017d9727366f4764
Ben Hardy <hardybe92@gmail.com>
2021-12-15 15:35:22 -0600

    nevermind. I figured out my issue almost immediately. then I managed to finish both parts in like 5 minutes. I forgot to set up my for-loop of folding instructions to update the dimensions of the incoming matrix for each iteration of the fold function, which was causing a boundary issue. once that was fixed it was smooth sailing.

--- day13/day13.py
+++ day13/day13.py
@@ -3,9 +3,8 @@ import numpy as np
 
 
 def fold(fold_instructions, paper, old_x, old_y):
-	#print(fold_instructions)
 	
-	if fold_instructions[0] == 'y' and fold_instructions[1] >= old_x//2:
+	if fold_instructions[0] == 'y':
 		points_below_fold = deque()
 		points_above_fold = deque()
 		for y in range(0, fold_instructions[1]):
@@ -17,12 +16,10 @@ def fold(fold_instructions, paper, old_x, old_y):
 			for x in range(paper.shape[1]):
 				if paper[y, x] == 1:
 					points_below_fold.append([y, x])
-		#print(points_below_fold)
 		
 		for point in points_below_fold:
 			dist_from_fold = abs(fold_instructions[1] - point[0])
 			point[0] = fold_instructions[1] - dist_from_fold
-		#print(points_below_fold)
 		new_paper = np.zeros((old_x - fold_instructions[1], old_y + 1), dtype=int)
 		
 		for point in points_below_fold:
@@ -32,35 +29,6 @@ def fold(fold_instructions, paper, old_x, old_y):
 			new_paper[point[0], point[1]] = 1
 		
 		return new_paper
-	elif fold_instructions[0] == 'y' and fold_instructions[1] < old_x//2:
-		points_below_fold = deque()
-		points_above_fold = deque()
-		for y in range(0, fold_instructions[1]):
-			for x in range(paper.shape[1]):
-				if paper[y, x] == 1:
-					points_above_fold.append([y, x])
-		
-		for y in range(fold_instructions[1], paper.shape[0]):
-			for x in range(paper.shape[1]):
-				if paper[y, x] == 1:
-					points_below_fold.append([y, x])
-		print(points_above_fold)
-		for point in points_above_fold:
-			original_value = point[0]
-			point[0] = fold_instructions[1] + (fold_instructions[1] - point[0])
-			
-		print(points_above_fold)
-		print(points_below_fold)
-		new_paper = np.zeros((old_x - fold_instructions[1], old_y + 1), dtype=int)
-		
-		for point in points_below_fold:
-			new_paper[point[0], point[1]] = 1
-		
-		for point in points_above_fold:
-			new_paper[point[0], point[1]] = 1
-		
-		return new_paper
-	
 	elif fold_instructions[0] == 'x':
 		points_before_fold = deque()
 		points_after_fold = deque()
@@ -69,18 +37,16 @@ def fold(fold_instructions, paper, old_x, old_y):
 			for x in range(0, fold_instructions[1]):
 				if paper[y, x] == 1:
 					points_before_fold.append([y, x])
-		print(points_before_fold)
 		for y in range(paper.shape[0]):
 			for x in range(fold_instructions[1], paper.shape[1]):
 				if paper[y, x] == 1:
 					points_after_fold.append([y, x])
 		
-		
 		for point in points_after_fold:
 			dist_from_fold = abs(fold_instructions[1] - point[1])
 			point[1] = fold_instructions[1] - dist_from_fold
 		
-		new_paper = np.zeros((old_y + 1, old_x - fold_instructions[1]), dtype=int)
+		new_paper = np.zeros((old_y + 1, old_x - (old_x - fold_instructions[1])), dtype=int)
 		print(new_paper.shape)
 		for point in points_before_fold:
 			new_paper[point[0], point[1]] = 1
@@ -96,24 +62,22 @@ with open('input.txt', 'r') as f:
 	
 	folds_start = -1
 	
-	points_to_mark = deque() # I just read about these, let's try them out
+	points_to_mark = deque()  # I just read about these, let's try them out
 	for i in range(len(lines)):
 		if lines[i] == '':
 			folds_start = i + 1
 			break
 		else:
-			x, y = lines[i].split(',')
-			points_to_mark.append([int(x),int(y)])
+			c, r = lines[i].split(',')
+			points_to_mark.append([int(c), int(r)])
 	# print(points_to_mark)
 	dim_y = max([i[0] for i in points_to_mark])
 	dim_x = max([i[1] for i in points_to_mark])
-	starting_paper = np.zeros((dim_x + 1, dim_y + 1), dtype=int)
+	cur_paper = np.zeros((dim_x + 1, dim_y + 1), dtype=int)
 
-	for point in points_to_mark:
-		starting_paper[point[1], point[0]] = 1
+	for p in points_to_mark:
+		cur_paper[p[1], p[0]] = 1
 		
-	# print(starting_paper)
-	
 	folds = deque()
 	
 	for i in range(folds_start, len(lines)):
@@ -121,5 +85,15 @@ with open('input.txt', 'r') as f:
 		direction = direction.split()[-1]
 		folds.append((direction, int(along)))
 	
-	finished_paper = fold(folds[0], starting_paper, dim_x, dim_y)
-	print(sum(finished_paper))
+	for fol in folds:
+		cur_paper = fold(fol, cur_paper, cur_paper.shape[0], cur_paper.shape[1])
+	
+	# for part 1
+	# print(np.sum(cur_paper))
+	
+	# for part 2
+	for r in range(cur_paper.shape[0]):
+		row = "["
+		for c in range(cur_paper.shape[1]):
+			row += " " + str(cur_paper[r, c])
+		print(row + ']')
