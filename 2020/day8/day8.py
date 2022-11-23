11683eea00af62953f839a1b1924bf2bcd042b86
Ben-Hardy <hardybe92@gmail.com>
2022-11-23 13:53:32 -0600

    finished day 8 part 1. will work on part 2 later

--- 2020/day8/day8.py
+++ 2020/day8/day8.py
@@ -6,8 +6,46 @@ class Operation:
 		self.number = number
 		self.was_used = False
 
+	def __str__(self):
+		return f"{self.instruction} {self.sign}{self.number} {self.was_used}"
 
-with open('input/smallinput.txt') as f:
+
+with open('input/input.txt') as f:
 	lines = f.read().split('\n')
-	print(lines)
-	line = lines[0]
+
+	operations = []
+	for line in lines:
+		chunks = line.split()
+		opsign = chunks[1][0]
+		num = chunks[1][1:]
+		operations.append(Operation(chunks[0], opsign, int(num)))
+
+	acc = 0
+	idx = 0
+
+	is_looping = True
+
+	while is_looping:
+		print(f"Current instruction: {operations[idx].instruction} {operations[idx].sign}{operations[idx].number}")
+		if operations[idx].was_used:
+			is_looping = False
+		elif operations[idx].instruction == 'nop':
+			operations[idx].was_used = True
+			idx += 1
+		elif operations[idx].instruction == 'acc':
+			if operations[idx].sign == '+':
+				acc += operations[idx].number
+			elif operations[idx].sign == '-':
+				acc -= operations[idx].number
+			operations[idx].was_used = True
+			idx += 1
+		elif operations[idx].instruction == 'jmp':
+			operations[idx].was_used = True
+			if operations[idx].sign == '+':
+				idx += operations[idx].number
+			elif operations[idx].sign == '-':
+				idx -= operations[idx].number
+
+
+
+	print(f'Final accumulator amount: {acc}')
