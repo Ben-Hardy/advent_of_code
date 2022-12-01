de75a2fcde125891546e04fcd8dbc068216c7544
Ben-Hardy <hardybe92@gmail.com>
2022-11-30 23:00:05 -0600

    changed script to use input args for simplicity

--- create_new_day.py
+++ create_new_day.py
@@ -1,29 +1,32 @@
 import os
+import sys
 
+if len(sys.argv) < 2:
+	print("Please give a year and a day")
+else:
+	year = int(sys.argv[1])
+	day = int(sys.argv[2])
 
-year = int(input("which year: "))
-day = int(input("which day: "))
+	directory = f"{year}/day{day}"
+	input_dir = f"{directory}/input"
 
-directory = f"{year}/day{day}"
-input_dir = f"{directory}/input"
+	os.makedirs(directory)
+	os.makedirs(input_dir)
 
-os.makedirs(directory)
-os.makedirs(input_dir)
+	input_path = f"{input_dir}/input.txt"
+	small_input_path = f"{input_dir}/smallinput.txt"
+	day_part_one = f"{directory}/day{day}.py"
+	day_part_two = f"{directory}/day{day}part2.py"
 
-input_path = f"{input_dir}/input.txt"
-small_input_path = f"{input_dir}/smallinput.txt"
-day_part_one = f"{directory}/day{day}.py"
-day_part_two = f"{directory}/day{day}part2.py"
-
-with open(input_path, 'x') as f:
-	print(f"{input_path} created!")
-	f.close()
-with open(small_input_path, 'x') as f:
-	print(f"{small_input_path} created!")
-	f.close()
-with open(day_part_one, 'x') as f:
-	print(f"{day_part_one} created!")
-	f.close()
-with open(day_part_two, 'x') as f:
-	print(f"{day_part_two} created!")
-	f.close()
+	with open(input_path, 'x') as f:
+		print(f"{input_path} created!")
+		f.close()
+	with open(small_input_path, 'x') as f:
+		print(f"{small_input_path} created!")
+		f.close()
+	with open(day_part_one, 'x') as f:
+		print(f"{day_part_one} created!")
+		f.close()
+	with open(day_part_two, 'x') as f:
+		print(f"{day_part_two} created!")
+		f.close()
