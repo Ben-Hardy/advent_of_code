6aacc660445119d27ac7b053fef284846f4c201e
Ben-Hardy <hardybe92@gmail.com>
2022-12-05 10:34:33 -0600

    did day 5. not my best work. might see if I can improve it

---
+++ 2022/day5/day5part2.py
@@ -0,0 +1,44 @@
+with open('input/input.txt', 'r') as f:
+	chunks = f.read().split('\n\n')
+	first_bit = chunks[0].split('\n')
+	num_columns = int(first_bit[-1].split()[-1])
+
+	print(chunks)
+	cols = [[] for _ in range(num_columns)]
+
+	for bit in first_bit[:-1]:
+		current = bit
+		col = 0
+		while len(current) > 0:
+			if current[:3] != '   ':
+				cols[col].append(current[1])
+			current = current[3:]
+			if len(current) > 0:
+				current = current[1:]
+			col += 1
+
+	print(cols)
+
+	second_bit = chunks[1].split('\n')
+
+	instructions = []
+	for bit in second_bit:
+		inst = bit.split()
+		instructions.append([int(inst[1]), int(inst[3]), int(inst[5])])
+
+	print(instructions)
+
+	for instruction in instructions:
+		num_to_move = instruction[0]
+		from_col = instruction[1]
+		to_col = instruction[2]
+
+		tmp_col = []
+		for _ in range(num_to_move):
+			tmp = cols[from_col - 1].pop(0)
+			tmp_col.append(tmp)
+		cols[to_col - 1] = tmp_col + cols[to_col - 1]
+
+	print(cols)
+
+	print(''.join(i[0] for i in cols if i))
